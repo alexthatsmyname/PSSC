10fb4588600b526cf8591337f77860ea09c16872
kertes <mihai.alex.c22@gmail.com>
2026-01-20 22:04:34 +0200

    Added table for Invoices

--- ProiectPSSC.Infrastructure/Migrations/20260120195830_AddInvoice.Designer.cs
+++ ProiectPSSC.Infrastructure/Migrations/20260120195830_AddInvoice.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProiectPSSC.Infrastructure.Persistence;
 
@@ -11,9 +12,10 @@ using ProiectPSSC.Infrastructure.Persistence;
 namespace ProiectPSSC.Infrastructure.Migrations
 {
     [DbContext(typeof(PsscDbContext))]
-    partial class PsscDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260120195830_AddInvoice")]
+    partial class AddInvoice
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ProiectPSSC.Infrastructure/Migrations/20260120195830_AddInvoice.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProiectPSSC.Infrastructure.Migrations
+{
+    public partial class AddInvoice : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Invoices",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    OrderId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IssuedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TotalAmount = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Invoices", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Invoices");
+        }
+    }
+}
